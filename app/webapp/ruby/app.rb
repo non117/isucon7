dc051f7ca1b07d8123e2ce1abf555680b41bbdf5
tyage <namatyage@gmail.com>
2017-10-22 19:30:56 +0900

    aaa

--- app/webapp/ruby/app.rb
+++ app/webapp/ruby/app.rb
@@ -322,7 +322,7 @@ class App < Sinatra::Base
     if !avatar_name.nil? && !avatar_data.nil?
       file_path = "/home/isucon/isubata/webapp/public/icons/#{avatar_name}"
       FileUtils.copy(file[:tempfile].path, file_path)
-      File.chmod(file_path, 644)
+      File.chmod(644, file_path)
       statement = db.prepare('UPDATE user SET avatar_icon = ? WHERE id = ?')
       statement.execute(avatar_name, user['id'])
       statement.close
